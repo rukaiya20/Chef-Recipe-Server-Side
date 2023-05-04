d3c38d469a7c6c4c494799fe921be74342ae9265
rukaiya20 <rukaiyajahantajin239@gmail.com>
2023-05-04 13:26:00 +0600

    igignore

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+// requires..
 const express = require('express')
 const app = express()
 const port =process.env.PORT || 5000
 const Chef = require('./Data/Chef.json')
 const Recipe = require('./Data/Recipe.json')
 const cors = require('cors')
+// ......................>
 
-
+// app get
 app.use(cors())
 
 app.get('/', (req, res) => {
@@ -17,16 +19,13 @@ app.get('/chef', (req, res) => {
 app.get('/recipe', (req, res) => {
   res.send(Recipe)
 })
+// ...............>
 
 app.get('/chef/:id', (req, res) => {
   const id = req.params.id;
   const selectedChef = Chef.find(n => n.id === id);
   res.send(selectedChef);
 })
-
-
-
-
 
 app.listen(port, () => {
   console.log(`Spice-Journey-Server in running on port ${port}`)
